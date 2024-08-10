316d5d09bfd2421f2ebc08bc86bb7ecc573f19ce
Antonio <pedroleon84659@gmail.com>
2024-08-09 20:46:03 -0500

    iniciando el proyecto

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import reactor.core.publisher.Mono;
+
+public class MethodosReactivos {
+    public void  Mono(){
+        Mono.just(new Persona("Pedro", 22)).subscribe(p -> log.info()
+    }
+    public static void main(String[] args) {
+
+    }
+}
